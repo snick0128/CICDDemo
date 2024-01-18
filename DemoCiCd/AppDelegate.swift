e88aa9c3677f20c5b18b6a4e49717a2ad94ef38c
vishalPris <vishal@prismetric.com>
2024-01-18 11:27:30 +0530

    Initial Commit

---
+++ DemoCiCd/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DemoCiCd
+//
+//  Created by Ashish Parmar on 18/01/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ DemoCiCd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoCiCd
+//
+//  Created by Ashish Parmar on 18/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Basic Exapmle of CI/CD")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
