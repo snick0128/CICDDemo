bc6ac312ce86d0b9f8d29c4911efbbb0ea7187db
vishalPris <vishal@prismetric.com>
2024-01-18 12:21:24 +0530

    Initial commit

--- DemoCiCd/ViewController.swift
+++ DemoCiCd/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Basic Exapmle of CI/CD")
+        print("Basic Exapmle of CI/CD")
+        print("Basic Exapmle of CI/CD")
         // Do any additional setup after loading the view.
     }
 
